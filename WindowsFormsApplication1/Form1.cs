e0aa2b4d20b41d6046d809b9356d61d45790a0a4
Jekyll <kalugin@outlook.com>
2020-12-20 12:20:06 +0300

    code beautified

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -4,25 +4,29 @@ using System.Drawing;
 using System.IO;
 using System.Net;
 using System.Net.Mail;
+using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
+
+using BirthdayReminder.Properties;
 
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
-        public bool commandLineArgs = false;
-
         private struct bdList
         {
-            public int daysTo;
-            public string message;
+            public int DaysTo;
+            public string Message;
         }
 
         public Form1(string[] cmdLine)
         {
             InitializeComponent();
-            if (cmdLine.Length == 1 && cmdLine[0] == "/email") BirthdayReminder.Properties.Settings.Default.SendEmail = true;
+            if (cmdLine != null && cmdLine.Length == 1 && cmdLine[0].ToLowerInvariant() == "/email")
+                Settings.Default.SendEmail = true;
+            else if (cmdLine.Length != 0)
+                MessageBox.Show("BirthdayReminder" + Environment.NewLine + "/email - send to e-mail");
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -30,137 +34,132 @@ namespace WindowsFormsApplication1
             FileStream inputFile;
             try
             {
-                inputFile = File.OpenRead(BirthdayReminder.Properties.Settings.Default.DatabaseFile);
+                inputFile = File.OpenRead(Settings.Default.DatabaseFile);
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error opening file:" + BirthdayReminder.Properties.Settings.Default.DatabaseFile + " : " + ex.Message);
+                MessageBox.Show("Error opening file:" + Settings.Default.DatabaseFile + " : " + ex.Message);
                 Application.Exit();
                 return;
             }
-            string inputStr = "";
-            List<bdList> bdListStr = new List<bdList>();
-            int c = 0;//inputFile.ReadByte();
-            DateTime now = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+
+            var bdListStr = new List<bdList>();
+            var c = 0; //inputFile.ReadByte();
+            var now = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
 
             //read CSV content string by string
             while (c != -1)
             {
-                int i = 0;
+                var i = 0;
                 c = 0;
-                inputStr = "";
+                var inputStr = "";
                 //while (i < colNum && c != -1)
                 while (c != '\r' && c != '\n' && c != -1)
                 {
                     c = inputFile.ReadByte();
-                    byte[] b = new byte[1];
+                    var b = new byte[1];
                     b[0] = (byte)c;
-                    if (c == BirthdayReminder.Properties.Settings.Default.CSVdelimiter) i++;
-                    if (c != -1) inputStr += Encoding.GetEncoding(BirthdayReminder.Properties.Settings.Default.CodePage).GetString(b);
+                    if (c == Settings.Default.CSVdelimiter) i++;
+                    if (c != -1) inputStr += Encoding.GetEncoding(Settings.Default.CodePage).GetString(b);
                 }
+
                 //while (c != '\r' && c != '\n' && c != -1) c = inputFile.ReadByte();
                 inputStr = inputStr.Trim().Replace("\r", "").Replace("\n", "");
-                if (inputStr != "")
+                if (inputStr == "") continue;
+
+                var cells = inputStr.Split(Settings.Default.CSVdelimiter);
+                if (cells.Length >= 2)
                 {
-                    string[] cells = inputStr.Split(BirthdayReminder.Properties.Settings.Default.CSVdelimiter);
-                    if (cells.Length >= 2)
+                    var bdDay = 0;
+                    var bdMonth = 0;
+                    var strYear = "";
+                    //get date
+                    var date = cells[0].Split(Settings.Default.DateDelimiter);
+                    for (i = 0; i < 3; i++)
+                        if (Settings.Default.DateFormat[i] == 'D')
+                            bdDay = int.Parse(date[i]);
+                        else if (Settings.Default.DateFormat[i] == 'M')
+                            bdMonth = int.Parse(date[i]);
+                        else if (Settings.Default.DateFormat[i] == 'Y') strYear = date[i];
+                    //calculate remaining days
+                    var birthday = new DateTime(now.Year, bdMonth, bdDay);
+                    var diff = (int)birthday.Subtract(now).TotalDays;
+                    if (diff < 0)
                     {
-                        int bd_day = 0;
-                        int bd_month = 0;
-                        int age = 0;
-                        string str_year = "";
-                        //get date
-                        string[] date = cells[0].Split(BirthdayReminder.Properties.Settings.Default.DateDelimiter);
-                        for (i = 0; i < 3; i++)
+                        birthday = new DateTime(DateTime.Now.Year + 1, bdMonth, bdDay);
+                        diff = (int)birthday.Subtract(now).TotalDays;
+                    }
+
+                    if (diff >= 0 && diff <= Settings.Default.WarnPeriod)
+                    {
+                        var daySign = "";
+                        var ageSign = "";
+
+                        var diffLast = diff - diff / 10;
+                        //it'll be right till the of 410 days
+                        if (diffLast == 0 || diffLast == 5 || diffLast == 6 || diffLast == 7 ||
+                            diffLast == 8 || diffLast == 9 || diff > 10 && diff < 15 ||
+                            diff > 110 && diff < 115 || diff > 210 && diff < 215 ||
+                            diff > 310 && diff < 315) daySign = " дней";
+                        else if (diffLast == 1) daySign = " день";
+                        else if (diffLast == 2 || diffLast == 3 || diffLast == 4) daySign = " дня";
+
+                        var tmpItem = new bdList
                         {
-                            if (BirthdayReminder.Properties.Settings.Default.DateFormat[i] == 'D')
-                            {
-                                bd_day = int.Parse(date[i]);
-                            }
-                            else if (BirthdayReminder.Properties.Settings.Default.DateFormat[i] == 'M')
-                            {
-                                bd_month = int.Parse(date[i]);
-                            }
-                            else if (BirthdayReminder.Properties.Settings.Default.DateFormat[i] == 'Y')
-                            {
-                                str_year = date[i];
-                            }
-                        }
-                        //calculate remaining days
-                        DateTime birthday = new DateTime(now.Year, bd_month, bd_day);
-                        int diff = (int)birthday.Subtract(now).TotalDays;
-                        if (diff < 0)
+                            DaysTo = diff
+                        };
+                        if (diff == 0)
+                            tmpItem.Message = "Сегодня ДР: " + cells[1] + " " + cells[0];
+                        else
+                            tmpItem.Message = "Через " + diff + daySign + " ДР: " + cells[1] + " " + cells[0];
+                        //  check if year has no '?'and calculate age
+                        if (!strYear.Contains("?"))
                         {
-                            birthday = new DateTime(DateTime.Now.Year + 1, bd_month, bd_day);
-                            diff = (int)birthday.Subtract(now).TotalDays;
+                            var age = DateTime.Now.Year - int.Parse(strYear);
+
+                            diffLast = age - age / 10;
+                            //it'll be right till the age of 210
+                            if (diffLast == 0 || diffLast == 5 || diffLast == 6 || diffLast == 7 ||
+                                diffLast == 8 || diffLast == 9 || age > 10 && age < 15 ||
+                                age > 110 && age < 115) ageSign = " лет";
+                            else if (diffLast == 1) ageSign = " год";
+                            else if (diffLast == 2 || diffLast == 3 || diffLast == 4) ageSign = " года";
+
+                            tmpItem.Message += "(" + age + ageSign + ")";
                         }
-                        if (diff >= 0 && diff <= BirthdayReminder.Properties.Settings.Default.WarnPeriod)
-                        {
 
-                            string daySign = "";
-                            string ageSign = "";
-
-                            int _diffLast = diff - (int)(diff / 10);
-                            //it'll be right till the of 410 days
-                            if (_diffLast == 0 || _diffLast == 5 || _diffLast == 6 || _diffLast == 7 || _diffLast == 8 || _diffLast == 9 || (diff > 10 && diff < 15) || (diff > 110 && diff < 115) || (diff > 210 && diff < 215) || (diff > 310 && diff < 315)) daySign = " дней";
-                            else if (_diffLast == 1) daySign = " день";
-                            else if (_diffLast == 2 || _diffLast == 3 || _diffLast == 4) daySign = " дня";
-
-                            bdList tmpItem = new bdList();
-                            tmpItem.daysTo = diff;
-                            if (diff == 0)
-                            {
-                                tmpItem.message = "Сегодня ДР: " + cells[1] + " " + cells[0];
-                            }
-                            else
-                            {
-                                tmpItem.message = "Через " + diff.ToString() + daySign + " ДР: " + cells[1] + " " + cells[0];
-                            }
-                            //  check if year has no '?'and calculate age
-                            if (!str_year.Contains("?"))
-                            {
-                                age = DateTime.Now.Year - int.Parse(str_year);
-
-                                _diffLast = age - (int)(age / 10);
-                                //it'll be right till the age of 210
-                                if (_diffLast == 0 || _diffLast == 5 || _diffLast == 6 || _diffLast == 7 || _diffLast == 8 || _diffLast == 9 || (age > 10 && age < 15) || (age > 110 && age < 115)) ageSign = " лет";
-                                else if (_diffLast == 1) ageSign = " год";
-                                else if (_diffLast == 2 || _diffLast == 3 || _diffLast == 4) ageSign = " года";
-
-                                tmpItem.message += "(" + age.ToString() + ageSign + ")";
-                            }
-                            tmpItem.message += ".\r\n";
-                            int n = 0;
-                            for (n = 0; n < bdListStr.Count; n++)
-                            {
-                                if (bdListStr[n].daysTo > tmpItem.daysTo) break;
-                            }
-                            if (n < bdListStr.Count)
-                            {
-                                bdListStr.Insert(n, tmpItem);
-                            }
-                            else bdListStr.Add(tmpItem);
-                        }
+                        tmpItem.Message += "."+ Environment.NewLine;
+                        var n = 0;
+                        for (n = 0; n < bdListStr.Count; n++)
+                            if (bdListStr[n].DaysTo > tmpItem.DaysTo)
+                                break;
+                        if (n < bdListStr.Count)
+                            bdListStr.Insert(n, tmpItem);
+                        else bdListStr.Add(tmpItem);
                     }
                 }
             }
+
             inputFile.Close();
             if (bdListStr.Count > 0)
             {
-                if (BirthdayReminder.Properties.Settings.Default.SendEmail == false)
+                if (Settings.Default.SendEmail == false)
                 {
-                    textBox_bdList.Font = new Font(textBox_bdList.Font.FontFamily, BirthdayReminder.Properties.Settings.Default.FontSize);
-                    for (int i = 0; i < bdListStr.Count; i++) textBox_bdList.Text += bdListStr[i].message;
+                    textBox_bdList.Font = new Font(textBox_bdList.Font.FontFamily, Settings.Default.FontSize);
+                    for (var i = 0; i < bdListStr.Count; i++) textBox_bdList.Text += bdListStr[i].Message;
                 }
-                else if (BirthdayReminder.Properties.Settings.Default.SendEmail == true)
+                else if (Settings.Default.SendEmail)
                 {
-                    string tmp = "";
-                    for (int i = 0; i < bdListStr.Count; i++) tmp += bdListStr[i].message;
+                    var tmp = "";
+                    for (var i = 0; i < bdListStr.Count; i++) tmp += bdListStr[i].Message;
                     SendEmail(tmp);
                     Application.Exit();
                 }
             }
-            else Application.Exit();
+            else
+            {
+                Application.Exit();
+            }
         }
 
         public void SendEmail(string mailMessage)
@@ -181,20 +180,22 @@ namespace WindowsFormsApplication1
             body += "</div>";*/
 
             // this is done using  using System.Net.Mail; & using System.Net; 
-            MailMessage mail = new MailMessage();
-            mail.From = new MailAddress(BirthdayReminder.Properties.Settings.Default.SmtpLogin);
-            mail.Subject = BirthdayReminder.Properties.Settings.Default.EmailSubject;
+            var mail = new MailMessage();
+            mail.From = new MailAddress(Settings.Default.SmtpLogin);
+            mail.Subject = Settings.Default.EmailSubject;
             mail.Body = mailMessage;
             mail.IsBodyHtml = false; // Can set to false, if you are sending pure text.
-            SmtpClient smtp = new SmtpClient(BirthdayReminder.Properties.Settings.Default.SmtpAddress, BirthdayReminder.Properties.Settings.Default.SmtpPort);
-            smtp.Credentials = new NetworkCredential(BirthdayReminder.Properties.Settings.Default.SmtpLogin, BirthdayReminder.Properties.Settings.Default.SmtpPassword);
-            smtp.EnableSsl = BirthdayReminder.Properties.Settings.Default.SmtpSSL;
+            var smtp = new SmtpClient(Settings.Default.SmtpAddress, Settings.Default.SmtpPort);
+            smtp.Credentials = new NetworkCredential(Settings.Default.SmtpLogin, Settings.Default.SmtpPassword);
+            smtp.EnableSsl = Settings.Default.SmtpSSL;
 
-            string[] mailList = BirthdayReminder.Properties.Settings.Default.EmailTo.Split(BirthdayReminder.Properties.Settings.Default.CSVdelimiter);
+            var mailList = Settings.Default.EmailTo.Split(Settings.Default.CSVdelimiter);
 
-            for (int i = 0; i < mailList.Length; i++)
-            {
-                if (mailList[i] == "" || mailList[i].Length < 3) ;
+            for (var i = 0; i < mailList.Length; i++)
+                if (mailList[i] == "" || mailList[i].Length < 3)
+                {
+                    ;
+                }
                 else
                 {
                     mail.To.Add(mailList[i]);
@@ -207,7 +208,6 @@ namespace WindowsFormsApplication1
                         MessageBox.Show("Error sending mail: " + Ex.Message);
                     }
                 }
-            }
         }
     }
 }
